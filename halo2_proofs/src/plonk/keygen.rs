jtguibas/halo2-pse

Add `plonk::evaluation` for prover optimization

halo2_proofs/src/plonk/keygen.rs
@@ -10,15 +10,16 @@ use super::{
         Advice, Any, Assignment, Circuit, Column, ConstraintSystem, Fixed, FloorPlanner, Instance,
         Selector,
     },
-    permutation, Assigned, Error, LagrangeCoeff, Polynomial, ProvingKey, VerifyingKey,
+    evaluation::Evaluator,
+    permutation, Assigned, Error, Expression, LagrangeCoeff, Polynomial, ProvingKey, VerifyingKey,
 };
 use crate::{
-    arithmetic::CurveAffine,
+    arithmetic::{parallelize, CurveAffine},
     circuit::Value,
     poly::{
         batch_invert_assigned,
         commitment::{Blind, CommitmentScheme, Params, MSM},
-        EvaluationDomain,
+        EvaluationDomain, Rotation,
     },
 };
 
@@ -322,14 +323,28 @@ where
     let l_last = vk.domain.lagrange_to_coeff(l_last);
     let l_last = vk.domain.coeff_to_extended(l_last);
 
+    // Compute l_active_row(X)
+    let one = Scheme::Scalar::one();
+    let mut l_active_row = vk.domain.empty_extended();
+    parallelize(&mut l_active_row, |values, start| {
+        for (i, value) in values.iter_mut().enumerate() {
+            let idx = i + start;
+            *value = one - (l_last[idx] + l_blind[idx]);
+        }
+    });
+
+    // Compute the optimized evaluation data structure
+    let ev = Evaluator::new(&vk.cs);
+
     Ok(ProvingKey {
         vk,
         l0,
-        l_blind,
         l_last,
+        l_active_row,
         fixed_values: fixed,
         fixed_polys,
         fixed_cosets,
         permutation: permutation_pk,
+        ev,
     })
 }

kroma-network/halo2

halo2_proofs/src/plonk/keygen.rs
@@ -10,10 +10,11 @@ use super::{
         Advice, Any, Assignment, Circuit, Column, ConstraintSystem, Fixed, FloorPlanner, Instance,
         Selector,
     },
-    permutation, Assigned, Error, LagrangeCoeff, Polynomial, ProvingKey, VerifyingKey,
+    evaluation::Evaluator,
+    permutation, Assigned, Error, Expression, LagrangeCoeff, Polynomial, ProvingKey, VerifyingKey,
 };
 use crate::{
-    arithmetic::CurveAffine,
+    arithmetic::{parallelize, CurveAffine},
     circuit::Value,
     poly::{
         batch_invert_assigned,
@@ -325,14 +326,28 @@ where
     let l_last = vk.domain.lagrange_to_coeff(l_last);
     let l_last = vk.domain.coeff_to_extended(l_last);
 
+    // Compute l_active_row(X)
+    let one = C::Scalar::one();
+    let mut l_active_row = vk.domain.empty_extended();
+    parallelize(&mut l_active_row, |values, start| {
+        for (i, value) in values.iter_mut().enumerate() {
+            let idx = i + start;
+            *value = one - (l_last[idx] + l_blind[idx]);
+        }
+    });
+
+    // Compute the optimized evaluation data structure
+    let ev = Evaluator::new(&vk.cs);
+
     Ok(ProvingKey {
         vk,
         l0,
-        l_blind,
         l_last,
+        l_active_row,
         fixed_values: fixed,
         fixed_polys,
         fixed_cosets,
         permutation: permutation_pk,
+        ev,
     })
 }