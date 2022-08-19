6a22e2c21f6769762be09974c872f163f6a4d039
ChihChengLiang <ChihChengLiang@users.noreply.github.com>
2022-08-19 08:00:29 +0000

    deploy: 2a19e441b1a29e08415e15338ad94bd6fff4a0ed

--- rustdoc/latest/implementors/halo2_proofs/arithmetic/trait.Field.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["halo2_proofs"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
