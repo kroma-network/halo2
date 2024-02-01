ad5dd91524c576ee92a37a1496706f8044563a41
Ryan Kim <ryan@lightscale.io>
2024-02-01 15:11:59 +0900

    feat(halo2_proofs): revert 62a4421
    Instead of replacing with MSM, the next commit tries to replace with
    whole Halo2 CPU code. This is a change for better gpu utilization ratio
    in the future.

--- halo2_proofs/build.rs
+++ halo2_proofs/build.rs
@@ -1,21 +1,9 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
-        .files([
-            "src/msm.cc",
-            #[cfg(feature = "tachyon_msm_gpu")]
-            "src/msm_gpu.cc",
-        ])
         .flag_if_supported("-std=c++17")
         .compile("halo2_proofs");
 
-    let dep_files = vec![
-        "src/lib.rs",
-        "src/msm.cc",
-        #[cfg(feature = "tachyon_msm_gpu")]
-        "src/msm_gpu.cc",
-        "include/msm.h",
-        "include/msm_gpu.h",
-    ];
+    let dep_files = vec!["src/lib.rs"];
     for file in dep_files {
         println!("cargo:rerun-if-changed={file}");
     }
